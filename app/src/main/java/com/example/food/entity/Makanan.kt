101fbdb58ee056b1fec9a6eb08992dff3c9dc3ff
emkr-13 <75230574+emkr-13@users.noreply.github.com>
2022-09-10 17:47:06 +0700

    Remove action bar + tambah Data recyliew

--- app/src/main/java/com/example/food/entity/Makanan.kt
+++ app/src/main/java/com/example/food/entity/Makanan.kt
@@ -5,6 +5,13 @@ package com.example.food.entity
         companion object{
             @JvmField
             var listOfMakanan = arrayOf(
+                Makanan("Nasi Lemak Ayam", "Rp.20.000"),
+                Makanan("Nasi Lemak Telur", "Rp.13.000"),
+                Makanan("Soto Sapi", "Rp.25.000"),
+                Makanan("Soto Ayam", "Rp.17.000"),
+                Makanan("Soto Makasar", "Rp.25.000"),
+                Makanan("Sate Ayam", "Rp.30.000"),
+                Makanan("Bubur Ayam", "Rp.27.000"),
                 Makanan("Nasi padang rendang", "Rp.20.000"),
                 Makanan("Nasi Padang ayam rendang", "Rp.13.000"),
                 Makanan("Nasi Padang daging cincang", "Rp.25.000"),
