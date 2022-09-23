9e89a4284b6a144cd02f0c0d2b018778f2ff0c26
emkr-13 <75230574+emkr-13@users.noreply.github.com>
2022-09-24 05:49:44 +0700

    fix

--- app/src/main/java/com/example/food/Register.kt
+++ app/src/main/java/com/example/food/Register.kt
@@ -10,79 +10,89 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
-import com.example.food.databinding.ActivityMainBinding
+import com.example.food.databinding.ActivityRegisterBinding
 import javax.xml.datatype.DatatypeConstants.MONTHS
 import java.util.*
 
 class Register : AppCompatActivity() {
-    private lateinit var BindingUser : ActivityMainBinding
-    private lateinit var registerLayout: ConstraintLayout
+    private lateinit var binding: ActivityRegisterBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        BindingUser = ActivityMainBinding.inflate(layoutInflater)
-        val viewBinding = BindingUser.root
-        setContentView(viewBinding)
+        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val  btnRegister=binding.btnRegister
+        val inputUsername=binding.ketikUsername
+        val inputPassword=binding.ketikPassword
+        val inputEmail=binding.ketikEmail
+        val inputTanggalLahir=binding.ketikTanggalLahir
+        val inputNomorHP=binding.ketikNomorHp
 
-        val cal = Calendar.getInstance()
-        val myYear = cal.get(Calendar.YEAR)
-        val myMonth = cal.get(Calendar.MONTH)
-        val myDay = cal.get(Calendar.DAY_OF_MONTH)
-        val intent = Intent(this, MainActivity::class.java)
-        BindingUser.btnLogin.setOnClickListener {
-            startActivity(intent)
-        }
 
-        BindingUser.btnRegister.setOnClickListener{
-            var  checkRegis = false
+        btnRegister.setOnClickListener {
+            var checkRegis = false
             val intent = Intent (this,MainActivity :: class.java)
 
-            val username :String = BindingUser.inputLayoutUsername.editText?.getText().toString()
-            val password : String = BindingUser.inputLayoutPassword.editText?.getText().toString()
-//            val email : String = BindingUser.inputLayoutEmail.editText().toString()
-//            val nomorHP=inputNomorHP.text.toString()
-//            val tanggalLahir=inputTanggalLahir.text.toString()
+
+
+            val username=inputUsername.text.toString()
+            val password=inputPassword.text.toString()
+            val email=inputEmail.text.toString()
+            val nomorHP=inputNomorHP.text.toString()
+            val tanggalLahir=inputTanggalLahir.text.toString()
+
+
 
             if (username.isEmpty()){
-                BindingUser.inputLayoutPassword.setError("Username must be filled with text")
+                inputUsername.setError("Username must be filled with text")
                 checkRegis = false
             }
             if (password.isEmpty()){
-                BindingUser.inputLayoutPassword.setError("Password must be filled with text")
+                inputPassword.setError("Password must be filled with text")
                 checkRegis = false
             }
-//            if (email.isEmpty()){
-//                inputEmail.setError("Email must be filled with text")
-//                checkRegis = false
-//            }
-//            if (nomorHP.isEmpty()){
-//                inputNomorHP.setError("nomorHP must be filled with text")
-//                checkRegis = false
-//            }
-//            if (tanggalLahir.isEmpty()){
-//                inputTanggalLahir.setError("Tanggal must be filled with text")
-//                checkRegis = false
-//            }
+            if (email.isEmpty()){
+                inputEmail.setError("Email must be filled with text")
+                checkRegis = false
+            }
+            if (nomorHP.isEmpty()){
+                inputNomorHP.setError("nomorHP must be filled with text")
+                checkRegis = false
+            }
+            if (tanggalLahir.isEmpty()){
+                inputTanggalLahir.setError("Tanggal must be filled with text")
+                checkRegis = false
+            }
             else{
                 checkRegis=true
             }
             if (!checkRegis)return@setOnClickListener
+
+
+
+
             val mBundle = Bundle()
-//
-//            mBundle.putString("username",inputUsername.text.toString())
-//            mBundle.putString("password",inputPassword.text.toString())
-//            mBundle.putString("email",inputEmail.text.toString())
-//            mBundle.putString("tanggalLahir",inputTanggalLahir.text.toString())
-//            mBundle.putString("nomorHp",inputNomorHP.text.toString())
+
+            mBundle.putString("username",inputUsername.text.toString())
+            mBundle.putString("password",inputPassword.text.toString())
+            mBundle.putString("email",inputEmail.text.toString())
+            mBundle.putString("tanggalLahir",inputTanggalLahir.text.toString())
+            mBundle.putString("nomorHp",inputNomorHP.text.toString())
 
             intent.putExtra("register", mBundle)
 
             startActivity(intent)
-
-        }
-
-
-
         }
     }
+
+//
+
+
+}
+
+
+
+
