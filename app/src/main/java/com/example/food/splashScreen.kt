fed68a98d143904166b3f7966d1f8e488d5316db
elmonzo139 <112062319+elmonzo139@users.noreply.github.com>
2022-09-23 14:05:03 +0700

    splash ulang

--- app/src/main/java/com/example/food/splashScreen.kt
+++ app/src/main/java/com/example/food/splashScreen.kt
@@ -7,7 +7,7 @@ import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("DEPRECATION")
-class SplashScreen : AppCompatActivity() {
+class splashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
