dd39d32ad5d3e760d28b21b4fd983a26ee996bdd
emkr-13 <75230574+emkr-13@users.noreply.github.com>
2022-09-24 20:45:32 +0700

    update Fragment

---
+++ app/src/main/java/com/example/food/EditProfil.kt
@@ -0,0 +1,11 @@
+package com.example.food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profil)
+    }
+}
---
+++ app/src/main/java/com/example/food/FragmentShowProfil.kt
@@ -0,0 +1,52 @@
+package com.example.food
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.example.food.user.UserDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class FragmentShowProfil : Fragment() {
+//    val db by lazy { activity?.let { UserDB(it) } }
+//
+//    var sharedPreferences: SharedPreferences? = null
+//
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_show_profil, container, false)
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        sharedPreferences = this.getActivity()?.getSharedPreferences("login", Context.MODE_PRIVATE)
+//        val showUsername: TextView = view.findViewById(R.id.showUser)
+//        val btnEdit: Button = view.findViewById(R.id.btnEdit)
+//        val id = sharedPreferences?.getString("id", "")
+//        CoroutineScope(Dispatchers.IO).launch {
+//            db?.userDao()?.getDataUser(id!!.toInt())
+//        }
+//
+//
+//
+//        btnEdit.setOnClickListener() {
+//            val intent = Intent(context, EditProfil::class.java)
+//            startActivity(intent)
+//        }
+//
+//
+//    }
+}
