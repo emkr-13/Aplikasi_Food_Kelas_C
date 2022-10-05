001cc531a980cd231d5de1fa001d4a0e285a75d8
TuaBangka12 <raynaldimangiri12@gmail.com>
2022-10-05 17:24:01 +0700

    update sekilas untuk tampilan home nya

--- app/src/main/java/com/example/food/FragmentHome.kt
+++ app/src/main/java/com/example/food/FragmentHome.kt
@@ -5,8 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.food.entity.Pesanan
 
 
 class FragmentHome : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        val adapter : RVHomeAdapter = RVHomeAdapter()
+
+    }
+
+
+    }
