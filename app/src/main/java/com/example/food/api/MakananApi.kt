2cc5e0c38b4dc6a9b7903a23b78bb213b4b9ee3f
TuaBangka12 <raynaldimangiri12@gmail.com>
2022-11-22 20:14:16 +0700

    Pembuatan Api

--- app/src/main/java/com/example/food/api/MakananApi.kt
+++ app/src/main/java/com/example/food/api/MakananApi.kt
@@ -3,7 +3,7 @@ package com.example.food.api
 class MakananApi {
     companion object {
 //        Isi Berdasarkan URL IP Anda
-        val BASE_URL = ""
+        val BASE_URL = "http://192.168.1.10/Aplikasi_food/ci4-apiserver/public/"
 
         val GET_ALL_URL = BASE_URL + "makanan/"
         val GET_BY_ID_URL = BASE_URL + "makanan/"
--- app/src/main/java/com/example/food/api/UserApi.kt
+++ app/src/main/java/com/example/food/api/UserApi.kt
@@ -3,7 +3,7 @@ package com.example.food.api
 class UserApi {
     companion object {
 //        isikan sesuai URl IP Anda
-        val BASE_URL = ""
+        val BASE_URL = "http://192.168.1.10/Aplikasi_food/ci4-apiserver/public/"
 
         val GET_ALL_URL = BASE_URL + "user/"
         val GET_BY_ID_URL = BASE_URL + "login/"
