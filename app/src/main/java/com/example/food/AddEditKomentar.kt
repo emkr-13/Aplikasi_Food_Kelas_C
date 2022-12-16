7db681e866aae5e745949d07d43537f0d0677c4d
emkr-13 <75230574+emkr-13@users.noreply.github.com>
2022-12-16 15:57:28 +0700

    Persiapan 2 CRUD tambahan

---
+++ app/src/main/java/com/example/food/AddEditKomentar.kt
@@ -0,0 +1,11 @@
+package com.example.food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditKomentar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_komentar)
+    }
+}
