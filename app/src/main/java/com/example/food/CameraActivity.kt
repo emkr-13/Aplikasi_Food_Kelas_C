8d8833a796522a2c7f72ccb0c2f6dfba5330ca28
emkr-13 <75230574+emkr-13@users.noreply.github.com>
2022-12-19 07:47:47 +0700

    update Camera bisa bolak balik

--- app/src/main/java/com/example/food/CameraActivity.kt
+++ app/src/main/java/com/example/food/CameraActivity.kt
@@ -12,42 +12,53 @@ import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.Toast
 import com.example.food.CameraView
+import java.io.IOException
 
 class CameraActivity : AppCompatActivity() {
     lateinit var proximitySensor: Sensor
     lateinit var sensorManager: SensorManager
+
     private var mCamera: Camera? = null
     private var mCameraView: CameraView? = null
+
+    private var currentCameraId: Int = Camera.CameraInfo.CAMERA_FACING_BACK
+    private val cameraBela: Int = Camera.CameraInfo.CAMERA_FACING_BACK
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         1
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
 
-        if(proximitySensor == null){
-            Toast.makeText(this, "No proximty sensor found in device...", Toast.LENGTH_SHORT).show()
+        try {
+            mCamera = Camera.open(currentCameraId)
+        }catch (e: IOException){
+            Log.d("Error", "Failed to get Camera" + e.message)
+        }
+
+        if (mCamera != null){
+            mCameraView = CameraView(this@CameraActivity, mCamera!!)
+            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
+            camera_view.addView(mCameraView)
+        }
+        @SuppressLint("MissingInflatedId", "LocalSuppress")
+        val imageClose = findViewById<View>(R.id.imgClose) as ImageButton
+        imageClose.setOnClickListener{view: View? -> System.exit(0)}
+
+
+        if (proximitySensor == null) {
+
+            Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
             finish()
-        }else{
+        } else {
+
             sensorManager.registerListener(
                 proximitySensorEventListener,
                 proximitySensor,
                 SensorManager.SENSOR_DELAY_NORMAL
             )
         }
-        try {
-            mCamera = Camera.open()
-        }catch (e:Exception){
-            Log.d("Error", "Failed to get Camera" + e.message)
-        }
-        if (mCamera != null){
-            mCameraView = CameraView(this, mCamera!!)
-            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
-            camera_view.addView(mCameraView)
-        }
-        @SuppressLint("MissingInflatedId", "LocalSuppress") val imageClose =
-            findViewById<View>(R.id.imgClose) as ImageButton
-        imageClose.setOnClickListener{view: View? -> System.exit(0)}
     }
 
     var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
@@ -56,23 +67,45 @@ class CameraActivity : AppCompatActivity() {
         }
 
         override fun onSensorChanged(event: SensorEvent) {
+
             if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
                 if (event.values[0] == 0f) {
+
+                    if (currentCameraId == cameraBela) {
+                        mCamera?.stopPreview();
+                    }
+                    mCamera?.release();
+
+
+                    if(currentCameraId == Camera.CameraInfo.CAMERA_FACING_BACK){
+                        currentCameraId = CAMERA_FACING_FRONT;
+                    }
+                    else {
+                        currentCameraId = Camera.CameraInfo.CAMERA_FACING_BACK;
+                    }
                     try {
-                        mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT)
-                    }catch (e:Exception){
+                        mCamera = Camera.open(currentCameraId)
+                    }catch (e: IOException){
                         Log.d("Error", "Failed to get Camera" + e.message)
                     }
+
+
+
                     if (mCamera != null){
                         mCameraView = CameraView(this@CameraActivity, mCamera!!)
                         val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
                         camera_view.addView(mCameraView)
                     }
-                    @SuppressLint("MissingInflatedId", "LocalSuppress") val imageClose =
-                        findViewById<View>(R.id.imgClose) as ImageButton
+                    @SuppressLint("MissingInflatedId", "LocalSuppress")
+                    val imageClose = findViewById<View>(R.id.imgClose) as ImageButton
                     imageClose.setOnClickListener{view: View? -> System.exit(0)}
-                } else {
                 }
+
+
+
+            } else {
+
+
             }
         }
     }
